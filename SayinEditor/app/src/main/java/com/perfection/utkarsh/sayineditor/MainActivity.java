9c29590943e5c9a4775bae3e079fa39f4c602609
utkarshsingh99 <utkarshsingh369@gmail.com>
2018-03-24 12:44:28 +0530

    Added some utilities

--- SayinEditor/app/src/main/java/com/perfection/utkarsh/sayineditor/MainActivity.java
+++ SayinEditor/app/src/main/java/com/perfection/utkarsh/sayineditor/MainActivity.java
@@ -1,12 +1,14 @@
 package com.perfection.utkarsh.sayineditor;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,6 +18,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
